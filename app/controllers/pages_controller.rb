dcc788259f0893ee7ec4fd5a4af9f6d3753f6671
Auriga <area511alienware@gmail.com>
2016-11-06 17:08:39 +0000

    Basic model code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,29 @@
 class PagesController < ApplicationController
   def index
+    @users = User.all
   end
-
+  def connections
+    @users = User.all
+  end
   def home
   end
 
   def support
+  end
+  
+  def upvote
+    #user_id: current_user.id
+   # @user = User.find_by_id(params[:id])
+  #  @user.upvote_by current_user
+    redirect_to :back
+  end
+  
+  def downvote
+ #   @user = User.find_by_id(params[:id])
+ #   @user.downvote_by current_user
+    redirect_to :back
+  end 
+  def user_params
+      params.require(:user).permit(:bio, :name, :zipcode, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users do
+    member do
+      put "like",    to:    "pages#upvote"
+      put "dislike", to: "pages#downvote"
+    end
+  end
   root 'pages#index'
   get 'home'  => 'pages#home'
   get 'support' => 'pages#support'
-
+  get 'connections' => 'pages#connections'
 end
