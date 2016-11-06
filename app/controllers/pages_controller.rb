94a281f8109a66c96947eecf82545b7b70784485
Auriga <area511alienware@gmail.com>
2016-11-06 18:23:56 +0000

    Cleaning up for the template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @users = User.all
-  end
-  def connections
-    @users = User.all
+   
   end
   def home
   end
@@ -11,19 +8,7 @@ class PagesController < ApplicationController
   def support
   end
   
-  def upvote
-    #user_id: current_user.id
-   # @user = User.find_by_id(params[:id])
-  #  @user.upvote_by current_user
-    redirect_to :back
-  end
-  
-  def downvote
- #   @user = User.find_by_id(params[:id])
- #   @user.downvote_by current_user
-    redirect_to :back
-  end 
   def user_params
-      params.require(:user).permit(:bio, :name, :zipcode, :email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
